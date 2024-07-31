1e70f120e1a579abd47fb199ef498167bd968726
abdoutalby <abderrahmentalby@gmail.com>
2024-07-31 08:33:07 +0100

    setting up admin accoun on startup

--- src/main/java/com/uib/donation/DonationApplication.java
+++ src/main/java/com/uib/donation/DonationApplication.java
@@ -1,9 +1,18 @@
 package com.uib.donation;
 
+import com.uib.donation.models.User;
+import com.uib.donation.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -13,11 +22,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				description = "API documentation"
 		)
 )
-public class DonationApplication {
+public class DonationApplication  implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(DonationApplication.class);
+	@Autowired
+private UserRepository userRepository;
+
+@Autowired
+private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DonationApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Optional<User> optionalUser = userRepository.findByEmail("admin@admin.com") ;
+		if(optionalUser.isEmpty()){
+		User user = new User();
+		user.setEmail("admin@admin.com");
+		user.setPassword(passwordEncoder.encode("admin"));
+		user.setUserType(User.UserType.ADMIN);
+		user.setUsername("admin");
+		user.setStatus(User.UserStatus.ENABLED);
+		user.setName("admin");
+		userRepository.save(user);
+		log.info("admin account setup succ");
+	} else {
+		log.info("admin already exist ");
+		}
+	}
 }
